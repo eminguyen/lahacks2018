6026ddab4f1e83df9fa0c74e1cf3cea763a8132b
Linda <32447325+Linda-Yang@users.noreply.github.com>
2018-03-31 03:24:24 -0700

    gl kids v2

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+//sets up express server
 var express = require("express");
+//constructs the server
 var app = express();
+//detect working directory
 var path = require("path");
 
+//finds public
 app.use(express.static(path.join(__dirname, "public")));
 
+//runs server on port 3001 
 app.listen(3001, function(){console.log("hello server")});
 
+//landing page?? like an airplane --> serves up html
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/meme', function(req, res) {
+    res.send("meme");
 })
