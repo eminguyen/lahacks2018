bd1174add78ddee1584d447fd0895ea1d54cd3c5
Linda Yang <Linda@Lindas-Computer.local>
2018-03-31 02:40:11 -0700

    added timer

---
+++ assets/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    console.log("hello kids!");
+    //This method starts the timer
+    function startTimer() {
+        var timer = 10;
+        $("#time").text(timer);
+        var counter = setInterval(countdown, 1000);
+        //This method updates the timer 
+        function countdown() {
+            timer--;
+            if (timer >= 0) {
+                $("#time").text(timer);
+            }
+            else {
+                clearInterval(counter);
+                newMeme();
+                return;
+            }
+        }
+    }
+    //This method is called each time a new meme is presented
+    function newMeme() {
+        startTimer();
+        
+    }
+    newMeme();
+
+
+
+
+})
+
+
+
