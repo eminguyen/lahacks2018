6df08bddd7dd79f0a1b648941d14cfe8ec589006
eminnguyen <emilyhuongnguyen@gmail.com>
2018-03-31 05:03:01 -0700

    Add meme scraper

---
+++ scrape.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+const JSDOM = require('jsdom').JSDOM
+
+let url = 'https://me.me/';
+var i;
+var list = [];
+
+fetch(url)
+  .then(resp => resp.text())
+  .then(text => {
+    let dom = new JSDOM(text)
+    let { document } = dom.window;
+    let tempList = document.getElementsByTagName("img");
+    for(i = 0; i < tempList.length; i++ ) {
+      var link = (tempList[i].getAttribute("src"));
+      if (link.startsWith("https")){
+        list.push(link);
+      }
+    }
+    console.log(list);
+    module.exports = list;
+   })
