4169787789702bab42babc56b58c256c25675e63
Clark <clarkphan@github.com>
2018-03-31 22:13:24 -0700

    better meme logic

--- public/assets/game.js
+++ public/assets/game.js
@@ -2,26 +2,60 @@ $(document).ready(function(){
     console.log("hello kids!");
     //This method starts the timer
     var counter;
+    var timer;
+    var userAnswers = [];
+    
+    var i = -1;
     function startTimer() {
-        var timer = 10;
+        timer = 10;
         $("#time").text(timer);
         counter = setInterval(countdown, 1000);
         //This method updates the timer
         function countdown() {
-            timer--;
-            if (timer >= 0) {
+            if (timer > 0) {
+                timer--;
                 $("#time").text(timer)
             }
             else {
-                clearInterval(counter);
+                newMeme();
                 return;
             }
         }
     }
+
+    function getTimeLeft() {
+        return timer;
+    }
+
     //This method is called each time a new meme is presented
     function newMeme() {
+        document.getElementById('memeImg').src = images[i >= images.length - 1 ? i = 0 : ++i];
+        if(i===images.length-1){
+            var myNode = document.getElementById('outerDiv');
+            myNode.innerHTML='';
+  
+            var newNode = document.createElement('div');
+            newNode.innerHTML = "button1 clicked " + button1 + " times.";
+            myNode.appendChild(newNode);
+  
+            var newNode2 = document.createElement('div2');
+            newNode2.innerHTML = "button2 clicked " + button2 + " times.";
+            myNode.appendChild(newNode2);
+            myNode.appendChild(document.createElement("br"));
+  
+            var newNode3 = document.createElement('div3');
+            newNode3.innerHTML = "button3 clicked " + button3 + " times.";
+            myNode.appendChild(newNode3);
+  
+            myNode.setAttribute("align","center");
+            button1 = 0;
+            button2 = 0;
+            button3 = 0;
+            return;
+          }
         clearInterval(counter);
         startTimer();
+
     }
 
     var images = ['http://i1.kym-cdn.com/photos/images/newsfeed/000/250/007/672.jpg',
@@ -29,8 +63,7 @@ $(document).ready(function(){
     'https://www.askideas.com/media/41/All-These-Flavors-And-You-Choose-To-Be-Salty-Funny-Girl-Meme-Picture.jpg',
     'https://c1.staticflickr.com/3/2200/32909417816_b3e9de59b0_b.jpg',
     'https://i.imgur.com/WA7uzXF.jpg',
-    'https://i.imgur.com/cYDnwFv.jpg'],
-    i = -1;
+    'https://i.imgur.com/cYDnwFv.jpg']
 
     // preload
   /*  for (var j=images.length; j--;) {
@@ -40,6 +73,10 @@ $(document).ready(function(){
     var button1 = 0;
     var button2 = 0;
     var button3 = 0;
+    
+    // socket io
+    var socket = io('http://localhost:3001');
+    
     // event handler
     $('.myButton').click(function(){
         var buttonclicked = this.id;
@@ -56,35 +93,20 @@ $(document).ready(function(){
         else{
           button3++;
         }
-        if(i===images.length-1){
-          var myNode = document.getElementById('outerDiv');
-          myNode.innerHTML='';
-
-          var newNode = document.createElement('div');
-          newNode.innerHTML = "button1 clicked " + button1 + " times.";
-          myNode.appendChild(newNode);
-
-          var newNode2 = document.createElement('div2');
-          newNode2.innerHTML = "button2 clicked " + button2 + " times.";
-          myNode.appendChild(newNode2);
-          myNode.appendChild(document.createElement("br"));
-
-          var newNode3 = document.createElement('div3');
-          newNode3.innerHTML = "button3 clicked " + button3 + " times.";
-          myNode.appendChild(newNode3);
-
-          myNode.setAttribute("align","center");
-          button1 = 0;
-          button2 = 0;
-          button3 = 0;
-          return;
-        }
-        document.getElementById('memeImg').src = images[i >= images.length - 1 ? i = 0 : ++i];
+        var timeLeft = getTimeLeft();
         newMeme();
+
+        var buttonData = {
+            userId: socket.id,
+            buttonClicked: buttonclicked,
+            timeLeft: timeLeft
+        }
+
+        userAnswers.push(buttonData);
+        console.log(userAnswers);
+        socket.emit('send button data', userAnswers);
     });
 
-    // socket io
-    var socket = io('http://localhost:3001');
     socket.on('check meme game', function(data) {
         console.log(data);
         var index = data.map(function(user) {
@@ -106,7 +128,7 @@ $(document).ready(function(){
     })
 
     socket.on('start meme game', function() {
-        newMeme();
+        startTimer();
         $('#gameStartModal').modal('show')
     })
 })
